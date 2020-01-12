72bee796bb758df801244310305b7f578c502c0c
zrz <316677100@qq.com>
2020-01-12 22:06:46 +0800

    "2020.1.12 Modify PersonalInfo"

--- dataBase/modPersonalInfo.hpp
+++ dataBase/modPersonalInfo.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream>
-// #include "../utility/convert/convertToNarrowChars.hpp"
 #include "mysql.h"
 // #pragma comment(lib, "libmysql.lib")
 #pragma comment(a, "libmysql.a")
