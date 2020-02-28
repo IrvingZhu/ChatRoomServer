f25f109c71c1360e3f8d88df2d9856fa9f7c1028
zrz <316677100@qq.com>
2020-02-28 15:07:36 +0800

    "2020.2.28 maintain the chat_message to session"

--- controller/chat/chat_message.hpp
+++ controller/chat/chat_message.hpp
@@ -22,7 +22,7 @@ private:
         max_body_length = 1024
     };
 
-    char dataBuf[max_user_length + max_body_length];
+    char dataBuf[max_user_length + 1 + max_body_length];
     size_t body_length; // length of data
     size_t user_length;
     // in this message format,header is length of
@@ -30,37 +30,41 @@ private:
 
     const char *body() const
     {
-        return this->dataBuf + max_user_length;
+        return this->dataBuf + this->user_length + 1;
     }
 
     char *body()
     {
-        return this->dataBuf + max_user_length;
+        return this->dataBuf + this->user_length + 1;
     }
 
     void struct_user(std::string username)
     {
         if(username.size() > max_user_length)
-        {
             username = username.substr(max_user_length);
-        }
+
         strcpy(this->data(), username.c_str());
     }
 
     void struct_info(std::string send_info)
     {
+        auto posi = this->user_length;
+        this->dataBuf[posi] = ':';
+
         if (send_info.size() > max_body_length) 
-        {
             send_info = send_info.substr(max_body_length);
-        }
+
         strcpy(this->body(), send_info.c_str());
     }
 
 public:
     chat_message(const std::string &username, const std::string &send_info) : body_length(send_info.size()),
-          user_length(max_user_length) 
+          user_length(username.size()) 
     {
         memset(this->dataBuf, 0, (max_user_length + max_body_length)*sizeof(char));
+
+        if(this->user_length > max_user_length)
+            this->user_length = max_user_length;
 
         if(this->body_length > max_body_length)
             this->body_length = max_body_length;
@@ -81,7 +85,7 @@ public:
 
     size_t length() const
     {
-        return user_length + body_length + 1;
+        return user_length + body_length + 2;
     }
 
     ~chat_message() {}
